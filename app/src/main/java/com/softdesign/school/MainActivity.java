f69d732525ac474ae32b1503e36a611fefce774e
mcflower <greenfild@gmail.com>
2016-01-25 20:43:00 +0400

    Выполнил 8 пункт дз

--- app/src/main/java/com/softdesign/school/MainActivity.java
+++ app/src/main/java/com/softdesign/school/MainActivity.java
@@ -1,5 +1,7 @@
 package com.softdesign.school;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.PersistableBundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +9,9 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,11 +22,17 @@ import com.softdesign.school.utils.Lg;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     public static final String VISIBLE_KEY = "visible";
+    public static final String COLOR_TOOLBAR = "color";
+    private int currentBackgroundToolBar;
 
     CheckBox mCheckBox;
     EditText mEditText2;
     Toolbar mToolBar;
 
+
+    Button mBtnBlue;
+    Button mBtnGreen;
+    Button mBtnRed;
 
 
     @Override
@@ -37,6 +48,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mEditText2 = (EditText) findViewById(R.id.editText2);
         mToolBar = (Toolbar) findViewById(R.id.toolbar);
+
+        mBtnBlue = (Button) findViewById(R.id.btn_blue);
+        mBtnGreen = (Button) findViewById(R.id.btn_green);
+        mBtnRed = (Button) findViewById(R.id.btn_red);
+
+        mBtnBlue.setOnClickListener(this);
+        mBtnGreen.setOnClickListener(this);
+        mBtnRed.setOnClickListener(this);
 
         setupToolbar();
 
@@ -108,7 +127,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     mEditText2.setVisibility(View.VISIBLE);
                 }
                 break;
+            case R.id.btn_blue:
+                //todo какой метод правильный (1) или (2)?
+//                mToolBar.setBackgroundColor(getResources().getColor(R.color.default_blue_color)); (1)
+                mToolBar.setBackgroundResource(R.color.default_blue_color);                       //(2)
+                currentBackgroundToolBar = R.color.default_blue_color;
+                setColorStatusBar(getResources().getColor(R.color.default_dark_blue_color));
+                break;
+            case R.id.btn_green:
+                mToolBar.setBackgroundResource(R.color.default_green_color);
+                currentBackgroundToolBar = R.color.default_green_color;
+                setColorStatusBar(getResources().getColor(R.color.default_dark_green_color));
+//                Toast.makeText(this, "" + mToolBar.getBackground().toString(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_red:
+                mToolBar.setBackgroundResource(R.color.default_red_color);
+                currentBackgroundToolBar = R.color.default_red_color;
+                setColorStatusBar(getResources().getColor(R.color.default_dark_red_color));
+                break;
         }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    protected void setColorStatusBar(int color){
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().setStatusBarColor(color);
     }
 
     @Override
@@ -116,14 +159,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onSaveInstanceState(outState);
         Lg.e(this.getLocalClassName(), "onSaveInstanceState()");
         outState.putBoolean(VISIBLE_KEY, mEditText2.getVisibility() == View.VISIBLE);
+        outState.putInt(COLOR_TOOLBAR, currentBackgroundToolBar);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         Lg.e(this.getLocalClassName(), "onRestoreInstanceState()");
-        int visibleState = savedInstanceState.getBoolean(VISIBLE_KEY) ? View.VISIBLE : View.INVISIBLE;
 
+        int visibleState = savedInstanceState.getBoolean(VISIBLE_KEY) ? View.VISIBLE : View.INVISIBLE;
         mEditText2.setVisibility(visibleState);
+
+        int backgroundToolBarColor = savedInstanceState.getInt(COLOR_TOOLBAR, R.color.default_blue_color);
+        mToolBar.setBackgroundResource(backgroundToolBarColor);
     }
 }
