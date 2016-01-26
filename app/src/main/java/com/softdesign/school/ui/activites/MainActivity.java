6db06540d0f8946d35439fa9af0baf553c880090
mcflower <greenfild@gmail.com>
2016-01-26 23:09:38 +0400

    Перенес класс MainActivity в папку с Activity Приступил к реализации navigationDrawer

--- app/src/main/java/com/softdesign/school/ui/activites/MainActivity.java
+++ app/src/main/java/com/softdesign/school/ui/activites/MainActivity.java
@@ -1,4 +1,4 @@
-package com.softdesign.school;
+package com.softdesign.school.ui.activites;
 
 import android.annotation.TargetApi;
 import android.os.Build;
@@ -14,6 +14,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.softdesign.school.R;
 import com.softdesign.school.utils.Lg;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
